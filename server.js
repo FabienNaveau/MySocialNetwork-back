f1362a58bde8443870f839c26c1c9a9b68d3c772
FabienNaveau <fabien.naveau@gmail.com>
2021-12-10 18:11:25 +0100

    Ajout des cors et déploiment heroku

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const client = require("./app/database");
 const userRoutes = require("./app/routes/userRoutes");
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
