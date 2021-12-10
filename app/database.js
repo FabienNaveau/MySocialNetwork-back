ea9e80e2f90dbc1d8fb857aa83a8c60678a7c9be
FabienNaveau <fabien.naveau@gmail.com>
2021-12-10 18:19:15 +0100

    Ajout variable environnement

--- app/database.js
+++ app/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const client = mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("Database connected"))
     .catch(error => console.log("Failed to connect to database", error))
 
